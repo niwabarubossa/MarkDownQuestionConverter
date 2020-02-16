3147930907bf69ec644b6675a3791b1f12827c2c
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-02-16 15:32:14 +0900

    connect custtom view file

---
+++ MapConvertQuestion/CustomView/MarkDownInput.swift
@@ -0,0 +1,16 @@
+//
+//  MarkDownInput.swift
+//  MapConvertQuestion
+//
+//  Created by 丹羽遼吾 on 2020/02/16.
+//  Copyright © 2020 ryogo.niwa. All rights reserved.
+//
+
+import UIKit
+
+class MarkDownInput: UIView {
+
+    @IBOutlet weak var inputTextView: UITextView!
+    @IBOutlet weak var indentAndLineButton: UIButton!
+    @IBOutlet weak var lineButton: UIButton!
+}
---
+++ MapConvertQuestion/CustomView/QuestionDidsplay.swift
@@ -0,0 +1,22 @@
+//
+//  QuestionDidsplay.swift
+//  MapConvertQuestion
+//
+//  Created by 丹羽遼吾 on 2020/02/16.
+//  Copyright © 2020 ryogo.niwa. All rights reserved.
+//
+
+import UIKit
+
+class QuestionDidsplay: UIView {
+
+    @IBOutlet weak var questionDisplayLabel: UILabel!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
