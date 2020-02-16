e6e753edd47309ddab66503398e484dd85cb01e6
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-02-16 19:35:57 +0900

    create nib files and storyboard

--- MapConvertQuestion/CustomView/MarkDownInput.swift
+++ MapConvertQuestion/CustomView/MarkDownInput.swift
@@ -13,4 +13,21 @@ class MarkDownInput: UIView {
     @IBOutlet weak var inputTextView: UITextView!
     @IBOutlet weak var indentAndLineButton: UIButton!
     @IBOutlet weak var lineButton: UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNib()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        loadNib()
+    }
+
+    func loadNib() {
+        if let view = Bundle(for: type(of: self)).loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?.first as? UIView {
+            view.frame = self.bounds
+            self.addSubview(view)
+        }
+    }
 }
---
+++ MapConvertQuestion/Model/QuestionModel.swift
@@ -0,0 +1,17 @@
+//
+//  Question.swift
+//  MapConvertQuestion
+//
+//  Created by 丹羽遼吾 on 2020/02/16.
+//  Copyright © 2020 ryogo.niwa. All rights reserved.
+//
+
+import Foundation
+
+struct QuestionModel{
+    var data:String
+    
+    init(data:String){
+        self.data = data
+    }
+}
