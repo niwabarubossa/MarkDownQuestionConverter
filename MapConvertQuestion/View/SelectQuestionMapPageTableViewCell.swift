e9c35c215d36229a03f1cb2a8e89084f4da8b360
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-03-01 11:27:44 +0900

    create select question map table view cell xib and .swift file

---
+++ MapConvertQuestion/View/SelectQuestionMapPageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SelectQuestionMapPageTableViewCell.swift
+//  MapConvertQuestion
+//
+//  Created by 丹羽遼吾 on 2020/03/01.
+//  Copyright © 2020 ryogo.niwa. All rights reserved.
+//
+
+import UIKit
+
+class SelectQuestionMapPageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
