3c33f32c48bf146231c54b1ec5dc46f3aac5166a
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-03-04 18:53:19 +0900

    create storyboard file and viewcontroller file

---
+++ MapConvertQuestion/View/ToDoQuestionPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ToDoQuestionPageViewController.swift
+//  MapConvertQuestion
+//
+//  Created by 丹羽遼吾 on 2020/03/04.
+//  Copyright © 2020 ryogo.niwa. All rights reserved.
+//
+
+import UIKit
+
+class ToDoQuestionPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
