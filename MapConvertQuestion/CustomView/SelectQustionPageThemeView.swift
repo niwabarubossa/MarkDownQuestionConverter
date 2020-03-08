f52334f1b515696324ab9fb995e8355630527d66
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-03-08 18:36:08 +0900

    pause

--- MapConvertQuestion/CustomView/SelectQustionPageThemeView.swift
+++ MapConvertQuestion/CustomView/SelectQustionPageThemeView.swift
@@ -9,13 +9,29 @@
 import UIKit
 
 class SelectQustionPageThemeView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    
+    @IBOutlet weak var themeImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNib()
     }
-    */
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        loadNib()
+    }
+
+    func loadNib() {
+        if let view = Bundle(for: type(of: self)).loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?.first as? UIView {
+            view.frame = self.bounds
+            self.addSubview(view)
+        }
+        self.themeImage.image = R.image.mapIcon()
+        let font = UIFont.systemFont(ofSize: 20, weight: UIFont.Weight(500))
+        self.titleLabel.font = font
+    }
 
 }
