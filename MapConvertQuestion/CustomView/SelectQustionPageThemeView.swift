a46e6c167a63971dcff62d9a8a0dabbbb1b66c5d
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-03-08 14:39:51 +0900

    xib file and .swift view file

---
+++ MapConvertQuestion/CustomView/SelectQustionPageThemeView.swift
@@ -0,0 +1,21 @@
+//
+//  SelectQustionPageThemeView.swift
+//  MapConvertQuestion
+//
+//  Created by 丹羽遼吾 on 2020/03/08.
+//  Copyright © 2020 ryogo.niwa. All rights reserved.
+//
+
+import UIKit
+
+class SelectQustionPageThemeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
