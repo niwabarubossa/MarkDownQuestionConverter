aebe12659cd4c48047e25e9d94f1619087e1379d
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-03-09 10:47:01 +0900

    add xib view

---
+++ MapConvertQuestion/CustomView/ButtonStackView.swift
@@ -0,0 +1,31 @@
+//
+//  ButtonStackView.swift
+//  MapConvertQuestion
+//
+//  Created by 丹羽遼吾 on 2020/03/09.
+//  Copyright © 2020 ryogo.niwa. All rights reserved.
+//
+
+import UIKit
+
+class ButtonStackView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNib()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        loadNib()
+    }
+
+    func loadNib() {
+        if let view = Bundle(for: type(of: self)).loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?.first as? UIView {
+            view.frame = self.bounds
+            self.addSubview(view)
+        }
+    }
+
+
+}
