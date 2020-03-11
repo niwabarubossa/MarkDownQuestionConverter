9cfb8b3c517aa4fcecf00b3c3a5610d307c7c37b
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-03-11 19:41:45 +0900

    add in tabbar item ok

--- MapConvertQuestion/Model/TutorialContentModel.swift
+++ MapConvertQuestion/Model/TutorialContentModel.swift
@@ -12,7 +12,7 @@ import UIKit
 struct TutorialContentModel{
     var title:String
     var content:String
-    var imageIcon:UIImage
+    var imageIcon:UIImage?
     
     init(title:String,content:String,imageIcon:UIImage){
         self.title = title
@@ -22,9 +22,9 @@ struct TutorialContentModel{
     
     static func createModels() -> [TutorialContentModel]{
         return [
-            TutorialContentModel(title: "first", content: "first content", imageIcon: UIImage(named: "brain")!),
-            TutorialContentModel(title: "second", content: "first content", imageIcon: UIImage(named: "brain")!),
-            TutorialContentModel(title: "third", content: "first content", imageIcon: UIImage(named: "brain")!)
+            TutorialContentModel(title: "first", content: "first content", imageIcon: R.image.student()!),
+            TutorialContentModel(title: "second", content: "first content", imageIcon: R.image.student()!),
+            TutorialContentModel(title: "third", content: "first content", imageIcon: R.image.student()!)
         ]
     }
 }
