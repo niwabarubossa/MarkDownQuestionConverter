f5ea5d3ddd77d1098c86341eb5f13ee767be3eb3
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-03-15 22:20:07 +0900

    uiview taped ok

--- MapConvertQuestion/View/SettingRootViewController.swift
+++ MapConvertQuestion/View/SettingRootViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class SettingRootViewController: UIViewController {
 
+    @IBOutlet weak var howToLabel: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.howToLabel.addGestureRecognizer((UITapGestureRecognizer(target: self, action: Selector("howToLabelTapped:"))))
+    }
+
+    @objc private func howToLabelTapped(_ sender:UIButton){
+        self.performSegue(withIdentifier: R.segue.settingRootViewController.goToHowToPage, sender: nil)
     }
 
 }
