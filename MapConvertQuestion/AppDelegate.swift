57afff5d32064b8ea810c54f64ccbf244a3df274
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-03-15 12:47:05 +0900

    model edit complete!

--- MapConvertQuestion/AppDelegate.swift
+++ MapConvertQuestion/AppDelegate.swift
@@ -31,7 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             print("２回目以降のログインです")
         }
         
-        let migSchemaVersion: UInt64 = 15
+        let migSchemaVersion: UInt64 = 16
         let config = Realm.Configuration(
             schemaVersion: migSchemaVersion,
             migrationBlock: { migration, oldSchemaVersion in
