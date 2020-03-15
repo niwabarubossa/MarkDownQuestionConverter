3962a55eff3ec448f0a7f4424624e6f481fc8278
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-03-15 11:32:16 +0900

    complet elabel oK!

---
+++ MapConvertQuestion/CustomView/ToDoQuestionCompleteLabel.swift
@@ -0,0 +1,32 @@
+//
+//  ToDoQuestionCompleteLabel.swift
+//  MapConvertQuestion
+//
+//  Created by 丹羽遼吾 on 2020/03/15.
+//  Copyright © 2020 ryogo.niwa. All rights reserved.
+//
+
+import UIKit
+
+class ToDoQuestionCompleteLabel: UIView {
+
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNib()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        loadNib()
+    }
+
+    func loadNib() {
+        if let view = Bundle(for: type(of: self)).loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?.first as? UIView {
+            view.frame = self.bounds
+            self.addSubview(view)
+        }
+    }
+
+}
