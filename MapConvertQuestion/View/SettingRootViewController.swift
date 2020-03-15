ea36d490030ae39155f4cbd2a4cb12353e8b46eb
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-03-15 22:10:40 +0900

    settings root vc file create

---
+++ MapConvertQuestion/View/SettingRootViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingRootViewController.swift
+//  MapConvertQuestion
+//
+//  Created by 丹羽遼吾 on 2020/03/15.
+//  Copyright © 2020 ryogo.niwa. All rights reserved.
+//
+
+import UIKit
+
+class SettingRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
