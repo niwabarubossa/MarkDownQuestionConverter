032820c2974104635d1df4a7cd54cd41065e1cba
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-04-12 10:56:36 +0900

    todo dashboard file create

---
+++ MapConvertQuestion/View/ToDoDashboardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ToDoDashboardViewController.swift
+//  MapConvertQuestion
+//
+//  Created by 丹羽遼吾 on 2020/04/12.
+//  Copyright © 2020 ryogo.niwa. All rights reserved.
+//
+
+import UIKit
+
+class ToDoDashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
