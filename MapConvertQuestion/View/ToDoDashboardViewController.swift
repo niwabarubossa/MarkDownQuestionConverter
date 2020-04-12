a76c7ceb1a7ca59e064fc54dc3fa1f9cb9d57d7b
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-04-12 12:47:15 +0900

    progress view ok

--- MapConvertQuestion/View/ToDoDashboardViewController.swift
+++ MapConvertQuestion/View/ToDoDashboardViewController.swift
@@ -7,24 +7,30 @@
 //
 
 import UIKit
+import MBCircularProgressBar
 
 class ToDoDashboardViewController: UIViewController {
 
+    
+    @IBOutlet weak var progressView: MBCircularProgressBarView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        progressView.value = 0
+        progressView.maxValue = 30
     }
-    */
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        UIView.animate(withDuration: 1.0) {
+            self.progressView.value = 15
+        }
+    }
 }
+
+
+
