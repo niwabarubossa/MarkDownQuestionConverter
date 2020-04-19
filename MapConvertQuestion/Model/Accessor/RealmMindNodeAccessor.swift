8e245d6acac8cfe1b6578a4df45d5c6e61a28dce
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-04-19 13:14:55 +0900

    singleton file create

---
+++ MapConvertQuestion/Model/Accessor/RealmMindNodeAccessor.swift
@@ -0,0 +1,15 @@
+//
+//  RealmMindNodeAccessor.swift
+//  MapConvertQuestion
+//
+//  Created by 丹羽遼吾 on 2020/04/19.
+//  Copyright © 2020 ryogo.niwa. All rights reserved.
+//
+
+import Foundation
+
+class RealmMindNodeAccessor {
+    static let sharedInstance = RealmMindNodeAccessor()
+    private init() {
+    }
+}
