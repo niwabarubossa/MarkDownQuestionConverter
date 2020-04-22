3646b69f13d85169761a4764f74cc762364d8366
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-04-22 15:00:43 +0900

    create user status display view controller

---
+++ MapConvertQuestion/View/UserStatusViewController.swift
@@ -0,0 +1,33 @@
+//
+//  UserStatusViewController.swift
+//  MapConvertQuestion
+//
+//  Created by 丹羽遼吾 on 2020/04/22.
+//  Copyright © 2020 ryogo.niwa. All rights reserved.
+//
+
+import UIKit
+
+class UserStatusViewController: UIViewController {
+
+    @IBOutlet weak var testScoreLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        testScoreLabel.text = "0"
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        UIView.animate(withDuration: 1.0) {
+            testScoreLabel.text = "asd"
+        }
+    }
+
+
+
+}
