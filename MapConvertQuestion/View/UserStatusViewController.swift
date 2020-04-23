df09e3d75260f37aac17764461640a5b6ad1ff4f
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-04-23 21:54:23 +0900

    abandn

--- MapConvertQuestion/View/UserStatusViewController.swift
+++ MapConvertQuestion/View/UserStatusViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class UserStatusViewController: UIViewController {
-
+    typealias CompletionClosure = ((_ result:Int) -> Void)
     var experienceDelta = 0.01
     @IBOutlet weak var progressView: UIProgressView!
     
@@ -20,18 +20,39 @@ class UserStatusViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.progressView.setProgress(Float(0.00 + self.experienceDelta), animated: true)
-        }
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.progressView.setProgress(Float(0.00 + self.experienceDelta), animated: true)
+//            self.progressView.setProgress(Float(0.00 + self.experienceDelta), animated: true)
+            for _ in 0..<2{
+                self.levelUpAnimation()
+            }
         }
     }
+}
 
+extension UserStatusViewController{
+    func levelUpAnimation() {
+        // ①
+        oneLevelUp(completionClosure: { (result:Int) in
+            // ⑥ 10が返ってくる
+            self.progressView.setProgress(Float(0.0), animated: false)
+        })
 
+        // ④
+    }
+    func oneLevelUp(completionClosure:@escaping CompletionClosure) {
+        // ②
 
+        UIView.animate(withDuration: 0.3, animations: {
+          self.progressView.setProgress(Float(0.99), animated: true)
+        }, completion: { (finished) in
+            // ⑤
+            // 終了した時、10という値を返す
+            completionClosure(10)
+        })
+        // ③
+    }
 }
