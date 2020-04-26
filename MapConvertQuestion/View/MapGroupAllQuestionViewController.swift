a0f142dcf6ff81249eeeadec19f1ff947af36dc4
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-04-26 10:12:09 +0900

    create and connect file

---
+++ MapConvertQuestion/View/MapGroupAllQuestionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapGroupAllQuestionViewController.swift
+//  MapConvertQuestion
+//
+//  Created by 丹羽遼吾 on 2020/04/26.
+//  Copyright © 2020 ryogo.niwa. All rights reserved.
+//
+
+import UIKit
+
+class MapGroupAllQuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
