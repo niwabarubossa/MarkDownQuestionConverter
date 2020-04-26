77f562a8c10ced8c5e71361977a3dca343e5847d
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-04-26 13:21:42 +0900

    create cell file

---
+++ MapConvertQuestion/CustomView/SimpleTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  SimpleTableViewCell.swift
+//  MapConvertQuestion
+//
+//  Created by 丹羽遼吾 on 2020/04/26.
+//  Copyright © 2020 ryogo.niwa. All rights reserved.
+//
+
+import UIKit
+
+class SimpleTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.setUpCell()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        self.setUpCell()
+    }
+    
+    private func setUpCell(){
+        titleLabel.font = LetGroup.baseFontName
+    }
+}
