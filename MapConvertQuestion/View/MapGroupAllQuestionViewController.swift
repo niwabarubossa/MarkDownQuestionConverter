c563744bf8eecf162cff939b317084012d57122b
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-04-26 13:27:58 +0900

    table view setup ok

--- MapConvertQuestion/View/MapGroupAllQuestionViewController.swift
+++ MapConvertQuestion/View/MapGroupAllQuestionViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class MapGroupAllQuestionViewController: UINavigationController {
 
-    @IBOutlet weak var tableVIew: UITableView!
+    let mindNodeShared = RealmMindNodeAccessor.sharedInstance
+    var dataSource:[Dictionary<String,String>] = [
+        ["test":"test"],
+        ["test":"test"],
+        ["test":"test"],
+    ]
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //viewdidload内部
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.register(SimpleTableViewCell.createXib(), forCellReuseIdentifier: SimpleTableViewCell.className)
+    }
+}
+
+extension MapGroupAllQuestionViewController: UITableViewDataSource,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: SimpleTableViewCell.className, for: indexPath ) as! SimpleTableViewCell
+        cell.selectionStyle = .none
+        return cell
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
 }
