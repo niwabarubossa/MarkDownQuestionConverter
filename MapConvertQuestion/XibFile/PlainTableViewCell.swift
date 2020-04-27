745a7a3fefb348919c26d42fc1a96f7684c3bd30
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-04-27 14:37:32 +0900

    refactoring

--- MapConvertQuestion/XibFile/PlainTableViewCell.swift
+++ MapConvertQuestion/XibFile/PlainTableViewCell.swift
@@ -15,14 +15,11 @@ class PlainTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
         setupCell()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
         setupCell()
     }
     
