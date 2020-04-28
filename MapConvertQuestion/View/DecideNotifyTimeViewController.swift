0b60c9567131ec50a93086d8b98936d89e23181a
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-04-28 20:13:00 +0900

    add files

---
+++ MapConvertQuestion/View/DecideNotifyTimeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DecideNotifyTimeViewController.swift
+//  MapConvertQuestion
+//
+//  Created by 丹羽遼吾 on 2020/04/28.
+//  Copyright © 2020 ryogo.niwa. All rights reserved.
+//
+
+import UIKit
+
+class DecideNotifyTimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
