48e80111d95c8c24c28ca66939c947ecbb2104de
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-04-28 20:04:07 +0900

    add image files

--- MapConvertQuestion/View/MyPageViewController.swift
+++ MapConvertQuestion/View/MyPageViewController.swift
@@ -15,9 +15,6 @@ class MyPageViewController: UIViewController {
     var presenter:MyPagePresenter!
     @IBOutlet weak var barChartView: BarChartView!
     @IBOutlet weak var totalAnswerTimesLabel: UILabel!
-    @IBOutlet weak var howToLabel: UIView!
-    
-    @IBOutlet weak var howToTextLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,9 +26,6 @@ class MyPageViewController: UIViewController {
     private func layout(){
         totalAnswerTimesLabel.text = "totalAnswerTimesLabel".localized
         totalAnswerTimesLabel.font = UIFont(name: LetGroup.baseFontName, size: 25)
-        self.howToTextLabel.text = "howToLabel".localized
-        self.howToTextLabel.font = UIFont(name: LetGroup.baseFontName, size: 20)
-        self.howToLabel.addGestureRecognizer((UITapGestureRecognizer(target: self, action: Selector(("howToLabelTapped:")))))
     }
     
     private func initializePage(){
@@ -47,12 +41,12 @@ class MyPageViewController: UIViewController {
     func notifyToPresenter(){
         presenter.presenterFunc()
     }
-
-    @objc private func howToLabelTapped(_ sender:UIButton){
+    
+    @IBAction func howToButtonTapped(_ sender: Any) {
         let howToVC = R.storyboard.tutorialExplanation.howToPage()
         self.present(howToVC!, animated: true, completion: nil)
+
     }
-    
     //presenter → view
     func viewFunc(){
         print("done from presenter function")
