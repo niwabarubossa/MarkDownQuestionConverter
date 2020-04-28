12f58c4a2090f98d6888ac386c657c9fe820b21f
丹羽遼吾 <niwabarubossa@yahoo.co.jp>
2020-04-28 21:42:34 +0900

    get notificatoin permission

--- MapConvertQuestion/View/DecideNotifyTimeViewController.swift
+++ MapConvertQuestion/View/DecideNotifyTimeViewController.swift
@@ -14,3 +14,30 @@ class DecideNotifyTimeViewController: UIViewController {
         super.viewDidLoad()
     }
 }
+
+extension DecideNotifyTimeViewController:UNUserNotificationCenterDelegate{
+    private func getNotificationGrant(){
+       if #available(iOS 10.0, *) {
+          // iOS 10
+          let center = UNUserNotificationCenter.current()
+          center.requestAuthorization(options: [.badge, .sound, .alert], completionHandler: { (granted, error) in
+              if error != nil { return }
+              if granted {
+                  print("通知許可")
+                  let center = UNUserNotificationCenter.current()
+                  center.delegate = self
+              } else { print("通知拒否") }
+          })
+      } else { // iOS 9以下
+          let settings = UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil)
+          UIApplication.shared.registerUserNotificationSettings(settings)
+      }
+    }
+
+}
+
+extension DecideNotifyTimeViewController:NotifyTimeDecideViewDelegate{
+    func submitButtonTapped() {
+        self.getNotificationGrant()
+    }
+}
